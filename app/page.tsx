4d4919df2d54e589151992c3ca4805f455e287f6
brocadeyu <yjb1725594062@gmail.com>
2024-10-03 18:28:17 +0800

    feat: change title

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Brocadeyu
       </h1>
-      <p className="mb-4">{`我是一个热爱代码和咖啡的前端开发工程师，擅长把创意变成炫酷网页。白天写代码，晚上捉bug，代码即生活，bug即乐趣。喜欢探索新技术，追求极致用户体验，期待与您共同创造精彩!`}</p>
+      <p className="mb-4">{`JavaScript FullStack & 三维可视化 & 独立app`}</p>
       <div className="my-8">
         <BlogPosts />
       </div>
